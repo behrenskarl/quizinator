4c652e4ad0c1b7d0316cba8895532171953b8d7e
DAGDAD <behrens.karl@gmail.com>
2020-07-05 19:52:47 -0700

    questions and answers appear in order, correct answer causes question to turn green, wrong answer turns red, score shown in percentage correctly, timer runs down correctly. no subtraction on timer working, quiz does not end, does not record high score in browser...

--- 02-Homework/questions.js
+++ 02-Homework/questions.js
@@ -1,5 +1,4 @@
-// Questions for quiz
-// $("#radio").prepend("<br><hr>" + questions);
+
 var start = document.getElementById("start-button");
 var quiz = document.getElementById("quiz");
 var question = document.getElementById("question");
@@ -15,7 +14,7 @@ var secondsDisplay = document.querySelector("#seconds");
 var startButton = document.querySelector("#start-button");
 
 start.addEventListener("click", startQuiz);
-// scoreDiv.addEventListener("click");
+
 
 var questions = [
     {
@@ -24,7 +23,7 @@ var questions = [
       choiceB: "Booleans",
       choiceC: "alerts",
       choiceD: "numbers",
-      answer: "alerts"
+      answer: "C"
     },
     {
       question: "The condition in an if / else statement is enclosed within ____.",
@@ -32,7 +31,7 @@ var questions = [
       choiceB: "curly brackets",
       choiceC: "parentheses",
       choiceD: "square brackets",
-      answer: "parentheses"
+      answer: "C"
     },
     {
       question: "Which event occurs when the user clicks on an HTML element?",
@@ -40,7 +39,7 @@ var questions = [
       choiceB: "onmouseclick",
       choiceC: "onfunction",
       choiceD: "click",
-      answer: "onclick"
+      answer: "A"
     },
     {
       question: "How do you declare a JavaScript variable?",
@@ -48,7 +47,7 @@ var questions = [
       choiceB: "var carName;",
       choiceC: "carname; var",
       choiceD: "var Carname:",
-      answer: "var carName;"
+      answer: "B"
     },
     {
       question: "How to do you create a function in JavaScript?",
@@ -56,7 +55,7 @@ var questions = [
       choiceB: "function = myFunction()",
       choiceC: "var function = _____",
       choiceD: "All of the above",
-      answer: "function myFunction()"
+      answer: "A"
     }
 ];
 
@@ -66,78 +65,9 @@ function startQuiz() {
     renderQuestions();  
     quiz.style.display = "block";
     runTimer();
+    renderProgress();
     renderQCounter();
 }
-
-var lastQuestion = questions.length - 1;
-var runningQuestion = 0;
-var qCount = 0;
-
-
-function renderQuestions() {
-    var q = questions[runningQuestion];
-    question.innerHTML = "<p>" + q.question + "</p>";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
-    choiceD.innerHTML = q.choiceD;
-}
-
-function renderProgress() {
-    for (var qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-      progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
-    }
-}
-
-function renderQCounter(){
-    if(qCount <= questionTime){
-        counter.innerHTML = qCount;
-        qCount++
-    }else{
-        count = 0;
-        // change progress color to red
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestions();
-        }else{
-            // end the quiz and show the score
-            clearInterval(timer);
-            scoreRender();
-        }
-    }
-}
-
-
-function checkAnswer(answer) {
-    if (answer == questions[runningQuestion].correct) {
-      answerIsCorrect();
-      scoreboard.score += 100;
-    } else {
-      answerIsWrong(); 
-    }
-    qCount = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestions();
-    }else{
-        // end the quiz and show the score
-        clearInterval(timer);
-        scoreRender();
-    }
-}
-
-
-
-function answerIsCorrect() {
-    document.getElementById("question").style.backgroundColor = "#0f0";
-}
-  //change wrong answer to red
-function answerIsWrong() {
-    document.getElementById("question").style.backgroundColor = "#f00";
-}
-
-
 //Timer variable and interval loop for timer
 var totalSeconds = 75;
 
@@ -154,17 +84,102 @@ function runTimer() {
     }, 1000);
 }
 
+var lastQuestion = questions.length - 1;
+var runningQuestion = 0;
+var qCount = 0;
+var questionNumber = 5;
+var score = 0;
+
+
+function renderQuestions() {
+    // document.getElementById("question").style.backgroundColor = "none";
+    var q = questions[runningQuestion];
+    question.innerHTML = "<p>" + q.question + "</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+    choiceD.innerHTML = q.choiceD;
+}
+
+// function renderProgress() {
+//     for (var qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+//       progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+//     }
+// }
+
+function renderQCounter(){
+    if(qCount <= questionNumber){
+        counter.innerHTML = qCount;
+        qCount++;
+    }else{
+        qCount = 0;
+        // change progress color to red
+        answerIsWrong();
+        if(runningQuestion < lastQuestion){
+            runningQuestion++;
+            renderQuestions();
+        }else{
+            // end the quiz and show the score
+            clearInterval(timer);
+            scoreRender();
+        }
+    }
+}
+
+
+
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].answer) {
+      answerIsCorrect();
+      score++;
+    } else {
+      answerIsWrong(); 
+    }
+    
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestions();
+    }else{
+        // end the quiz and show the score
+        clearInterval();
+        scoreRender();
+    }
+}
+
+
+
+function answerIsCorrect() {
+    document.getElementById("question").style.backgroundColor = "#0f0";
+}
+  //change wrong answer to red
+function answerIsWrong() {
+    document.getElementById("question").style.backgroundColor = "#f00";
+    totalSeconds - 15;
+}
+
+
+
+
 
 //scoreboard variables and functions
-var scoreboard = {
-    score: 0,
-    currentQuestion: 1,
-    history: [],
-  };
+// var scoreboard = {
+//     score = 0,
+//     history = []
+// }
+var history = [];
   
 function scoreRender() {
+    // localStorage.setItem("scoreContainer", JSON.stringify(history));
     scoreDiv.style.display = "block";
-    scoreDiv.textContent = scoreboard.history.length;
+    var scorePercent = Math.round(100 * score/questions.length);
+    scoreDiv.innerHTML += "<p>"+ scorePercent +"%</p>"; 
+} 
+
+
+// function scoreRender() {
+//     var scoreValue = scoreboard.score + qCount;
+//     scoreDiv.style.display = "block";
+//     scoreDiv.textContent = scoreboard.history.length;
   
-    history.pushState({score: scoreValue, qCount: 5});
-}
+//     history.pushState({score: scoreValue, qCount: 5});
+// }
